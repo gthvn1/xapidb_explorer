5872d8727c8b79d588dd4fbcf5e51d23cb3212cd
Guillaume <gthvn1@gmail.com>
2025-12-31 17:06:57 +0100

    first try for creating DbNode tree

--- src/xapidb/parser.rs
+++ src/xapidb/parser.rs
@@ -42,11 +42,11 @@ pub fn read_xml() -> DbNode {
 
     let mut buf = Vec::new();
 
-    let root = DbNode {
+    let mut stack: Vec<DbNode> = vec![DbNode {
         name: "root".to_string(),
         attributes: BTreeMap::new(),
         children: Vec::new(),
-    };
+    }];
 
     loop {
         match reader.read_event_into(&mut buf) {
@@ -54,39 +54,63 @@ pub fn read_xml() -> DbNode {
             Ok(Event::Eof) => break,
             Ok(Event::Start(e)) => {
                 // Start tag (with attributes) <tag attr="value">
-                println!("---- [start] begin -> {:?}", e.name());
-                dbg!(&e);
+                println!("---- [start] begin ");
+                let name = str::from_utf8(e.name().as_ref()).unwrap().to_string();
+                let mut attrs = BTreeMap::new();
+
                 for attr in e.attributes() {
-                    let attr = attr.expect("failed to parse attribute");
+                    let attr = attr.unwrap();
                     let key_bytes = attr.key.as_ref();
                     let key = str::from_utf8(key_bytes).expect("invalid utf8 key");
                     let value = attr
                         .unescape_value()
                         .expect("failed to unescape value")
                         .into_owned();
-                    println!("  ATTRIBUTE > {key} : {value}");
+                    attrs.insert(key.to_string(), value.to_string()).unwrap();
                 }
+                match name.as_str() {
+                    "table" => {
+                        // We are expecting to have only root in stack
+                        assert!(stack.len() == 1);
+
+                        // push it on the stack
+                        stack.push(DbNode {
+                            name: "table".to_string(),
+                            attributes: attrs,
+                            children: Vec::new(),
+                        });
+                    }
+                    "row" => {
+                        let row = DbNode {
+                            name: "row".to_string(),
+                            attributes: attrs,
+                            children: Vec::new(),
+                        };
+
+                        // Attach the row to the last node of the stack
+                        stack.last_mut().unwrap().children.push(row);
+                    }
+                    "manifest" | "database" => {}
+                    name => unreachable!("node {} is not expected", name),
+                }
+                dbg!(&e);
                 println!("---- [start] end");
             }
-            Ok(Event::End(e)) => {
+            Ok(Event::End(_)) => {
                 // End tag </tag>
-                println!("---- [end] begin");
-                dbg!(&e);
-                println!("---- [end] end");
+                stack.pop();
             }
             Ok(Event::Empty(e)) => {
                 // Empty element tag (with attributes) <tag attr="value" />
-                println!("---- [empty] begin -> {:?}", e.name());
-                dbg!(&e);
-                for attr in e.attributes() {
-                    let attr = attr.expect("failed to parse attribute");
-                    let key_bytes = attr.key.as_ref();
-                    let key = str::from_utf8(key_bytes).expect("invalid utf8 key");
-                    let value = attr
-                        .unescape_value()
-                        .expect("failed to unescape value")
-                        .into_owned();
-                    println!("  ATTRIBUTE > {key} : {value}");
+                let name = str::from_utf8(e.name().as_ref())
+                    .expect("invald utf8 name")
+                    .to_string();
+                println!("---- [empty] begin -> {}", name);
+                if name == "table" {
+                    let node = stack.pop().unwrap();
+                    // Add the node to the last node of the stack that we are expecting to be root
+                    assert!(stack.len() == 1);
+                    stack.last_mut().unwrap().children.push(node);
                 }
                 println!("---- [empty] end");
             }
@@ -102,6 +126,9 @@ pub fn read_xml() -> DbNode {
     }
 
     println!("done");
+
+    assert!(stack.len() == 1);
+    let root = stack.pop().unwrap();
     dbg!(&root);
     root
 }
