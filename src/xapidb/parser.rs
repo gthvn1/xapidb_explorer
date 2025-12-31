d315d620fc8f9702fa0dcaa33487f327dd80e62a
Guillaume <gthvn1@gmail.com>
2025-12-31 14:24:34 +0100

    Try to extract attributes

--- src/xapidb/parser.rs
+++ src/xapidb/parser.rs
@@ -1,8 +1,31 @@
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
+use std::collections::BTreeMap;
 use std::{fs::File, io::Read};
 
-pub fn read_xml() {
+// We want to end with a structure like
+//
+// name: root
+// attributes : []
+// children:
+//    +--> name: table
+//         attributes: "name" = "Cluster"
+//         children: []
+//    +--> name: table
+//         attributes: "name" = Driver_variant"
+//         children:
+//             +--> name: row
+//                  attributes: ["ref" = "...", "__ctime = ...", ...]
+//             ...
+
+#[derive(Debug)]
+pub struct DbNode {
+    pub name: String,
+    pub attributes: BTreeMap<String, String>,
+    pub children: Vec<DbNode>,
+}
+
+pub fn read_xml() -> DbNode {
     let fname = "examples/xapi-db.xml";
 
     let mut file = match File::open(fname) {
@@ -14,13 +37,16 @@ pub fn read_xml() {
     file.read_to_string(&mut contents)
         .expect("failed to read contents");
 
-    let first: String = contents.chars().take(20).collect();
-    println!("{}", first);
-
     let mut reader = Reader::from_str(&contents);
     reader.config_mut().trim_text(true);
 
     let mut buf = Vec::new();
+
+    let root = DbNode {
+        name: "root".to_string(),
+        attributes: BTreeMap::new(),
+        children: Vec::new(),
+    };
 
     loop {
         match reader.read_event_into(&mut buf) {
@@ -28,17 +54,41 @@ pub fn read_xml() {
             Ok(Event::Eof) => break,
             Ok(Event::Start(e)) => {
                 // Start tag (with attributes) <tag attr="value">
-                println!("found {:?}", e.name());
+                println!("---- [start] begin -> {:?}", e.name());
+                dbg!(&e);
+                for attr in e.attributes() {
+                    let attr = attr.expect("failed to parse attribute");
+                    let key_bytes = attr.key.as_ref();
+                    let key = str::from_utf8(key_bytes).expect("invalid utf8 key");
+                    let value = attr
+                        .unescape_value()
+                        .expect("failed to unescape value")
+                        .into_owned();
+                    println!("  ATTRIBUTE > {key} : {value}");
+                }
+                println!("---- [start] end");
             }
-            Ok(Event::End(_)) => {
+            Ok(Event::End(e)) => {
                 // End tag </tag>
-                println!("found end");
+                println!("---- [end] begin");
+                dbg!(&e);
+                println!("---- [end] end");
             }
             Ok(Event::Empty(e)) => {
                 // Empty element tag (with attributes) <tag attr="value" />
-                println!("---- empty begin");
-                dbg!(e);
-                println!("---- empty end");
+                println!("---- [empty] begin -> {:?}", e.name());
+                dbg!(&e);
+                for attr in e.attributes() {
+                    let attr = attr.expect("failed to parse attribute");
+                    let key_bytes = attr.key.as_ref();
+                    let key = str::from_utf8(key_bytes).expect("invalid utf8 key");
+                    let value = attr
+                        .unescape_value()
+                        .expect("failed to unescape value")
+                        .into_owned();
+                    println!("  ATTRIBUTE > {key} : {value}");
+                }
+                println!("---- [empty] end");
             }
             Ok(Event::Decl(_)) => {} // can be skipped
             // Other events are not expected in our DB so just panic
@@ -52,4 +102,6 @@ pub fn read_xml() {
     }
 
     println!("done");
+    dbg!(&root);
+    root
 }
