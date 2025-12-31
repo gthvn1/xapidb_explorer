d70f40f00021ff535f530ef5b3a6de40c339fe53
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-31 12:10:46 +0100

    Start to parse the database

--- src/xapidb/parser.rs
+++ src/xapidb/parser.rs
@@ -1,3 +1,5 @@
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
 use std::{fs::File, io::Read};
 
 pub fn read_xml() {
@@ -14,4 +16,34 @@ pub fn read_xml() {
 
     let first: String = contents.chars().take(20).collect();
     println!("{}", first);
+
+    let mut reader = Reader::from_str(&contents);
+    reader.config_mut().trim_text(true);
+
+    let mut buf = Vec::new();
+
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Err(e) => panic!("Error at position {}: {:?}", reader.error_position(), e),
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(e)) => println!("found {:?}", e.name()),
+            Ok(Event::End(_)) => println!("found end"),
+            Ok(Event::Empty(e)) => {
+                println!("---- empty begin");
+                dbg!(e);
+                println!("---- empty end");
+            }
+
+            Ok(Event::Decl(_)) => {} // can be skipped
+            // Other events are not expected in our DB
+            Ok(Event::Text(_)) => panic!("found text"),
+            Ok(Event::CData(_)) => panic!("found cdata"),
+            Ok(Event::Comment(_)) => panic!("found comment"),
+            Ok(Event::PI(_)) => panic!("found pi"),
+            Ok(Event::DocType(_)) => panic!("found doctype"),
+            Ok(Event::GeneralRef(_)) => panic!("found generalref"),
+        }
+    }
+
+    println!("done");
 }
