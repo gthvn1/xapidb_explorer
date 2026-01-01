1a0722eea6ac769d629c8eb7557824e013d6bba1
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-01 20:31:14 +0100

    Use List instead of table

--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,38 @@ use crossterm::event::{self, Event, KeyCode};
 
 use ratatui::{
     Frame,
-    layout::Constraint,
     style::Style,
-    widgets::{Block, Row, Table, TableState},
+    widgets::{Block, List, ListItem, ListState},
 };
 
 fn main() {
     let root = DbNode::read_xml();
     root.print_tree();
 
-    // ---- https://docs.rs/ratatui/latest/ratatui/widgets/struct.Table.html
-    let mut table_state = TableState::default();
-    table_state.select(Some(0));
-
-    let rows = [
-        Row::new(vec!["Cell11", "Cell12", "Cell13"]),
-        Row::new(vec!["Cell21", "Cell22", "Cell23"]),
-        Row::new(vec!["Cell31", "Cell32", "Cell33"]),
-    ];
+    // https://docs.rs/ratatui/latest/ratatui/widgets/struct.List.html
+    let items = ["Item 1", "Item 2", "Item 3"];
+    let mut state = ListState::default();
+    state.select(Some(0));
 
     let mut terminal = ratatui::init();
 
     loop {
         terminal
-            .draw(|frame| draw(frame, &rows, &mut table_state))
+            .draw(|frame| draw(frame, &items, &mut state))
             .expect("Failed to draw frame");
 
         match event::read().expect("failed to read event") {
             Event::Key(key) => match key.code {
                 KeyCode::Up => {
-                    let i = table_state.selected().unwrap_or(0);
+                    let i = state.selected().unwrap_or(0);
                     if i > 0 {
-                        table_state.select(Some(i - 1));
+                        state.select(Some(i - 1));
                     }
                 }
                 KeyCode::Down => {
-                    let i = table_state.selected().unwrap_or(0);
-                    if i + 1 < rows.len() {
-                        table_state.select(Some(i + 1));
+                    let i = state.selected().unwrap_or(0);
+                    if i + 1 < items.len() {
+                        state.select(Some(i + 1));
                     }
                 }
                 KeyCode::Esc | KeyCode::Char('q') => break,
@@ -54,17 +48,15 @@ fn main() {
     ratatui::restore();
 }
 
-fn draw(frame: &mut Frame, rows: &[Row], state: &mut TableState) {
-    let widths = [
-        Constraint::Length(5),
-        Constraint::Length(5),
-        Constraint::Length(10),
-    ];
+fn draw(frame: &mut Frame, items: &[&str], state: &mut ListState) {
+    let list_items: Vec<ListItem> = items.iter().map(|&i| ListItem::new(i)).collect();
+    let list = List::new(list_items)
+        .block(Block::bordered().title("List"))
+        .style(Style::new().white())
+        .highlight_style(Style::new().italic())
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true);
 
-    let table = Table::new(rows.to_vec(), widths)
-        .block(Block::new().title("Table"))
-        .row_highlight_style(Style::new().reversed())
-        .highlight_symbol(">>");
     let area = frame.area();
-    frame.render_stateful_widget(table, area, state);
+    frame.render_stateful_widget(list, area, state);
 }
