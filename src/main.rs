755bc105469ce1d094ce1a670264917167141279
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-01 17:09:46 +0100

    Hello world with ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use xapidb_explorer::xapidb::parser::DbNode;
 
+use crossterm::event::{self, Event};
+use ratatui::{Frame, text::Text};
+
 fn main() {
     let root = DbNode::read_xml();
     root.print_tree();
+
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw).expect("Failed to draw frame");
+        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
+            break;
+        }
+    }
+
+    ratatui::restore();
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello World!");
+    frame.render_widget(text, frame.area());
 }
