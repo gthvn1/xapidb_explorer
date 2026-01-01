b74c928271d6545f06fd888e48579789be230348
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-01 17:22:34 +0100

    Display a table

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use xapidb_explorer::xapidb::parser::DbNode;
 
 use crossterm::event::{self, Event};
-use ratatui::{Frame, text::Text};
+
+use ratatui::{
+    Frame,
+    layout::Constraint,
+    style::Style,
+    widgets::{Block, Row, Table, TableState},
+};
 
 fn main() {
     let root = DbNode::read_xml();
     root.print_tree();
 
+    // ---- https://docs.rs/ratatui/latest/ratatui/widgets/struct.Table.html
+    let mut table_state = TableState::default();
+    table_state.select(Some(0));
+
+    let rows = [
+        Row::new(vec!["Cell11", "Cell12", "Cell13"]),
+        Row::new(vec!["Cell21", "Cell22", "Cell23"]),
+        Row::new(vec!["Cell31", "Cell32", "Cell33"]),
+    ];
+
+    let widths = [
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(10),
+    ];
+
+    let table = Table::new(rows, widths)
+        .block(Block::new().title("Table"))
+        .row_highlight_style(Style::new().reversed())
+        .highlight_symbol(">>");
+
     let mut terminal = ratatui::init();
+
     loop {
-        terminal.draw(draw).expect("Failed to draw frame");
+        terminal
+            .draw(|frame| draw(frame, &table, &mut table_state))
+            .expect("Failed to draw frame");
         if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
             break;
         }
@@ -18,7 +48,7 @@ fn main() {
     ratatui::restore();
 }
 
-fn draw(frame: &mut Frame) {
-    let text = Text::raw("Hello World!");
-    frame.render_widget(text, frame.area());
+fn draw(frame: &mut Frame, table: &Table, state: &mut TableState) {
+    let area = frame.area();
+    frame.render_stateful_widget(table, area, state);
 }
