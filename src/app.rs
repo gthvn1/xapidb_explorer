b9405a5523a2525ceb543ff9881e9295153cbab8
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-02 16:30:10 +0100

    Move UI code in app.rs

---
+++ src/app.rs
@@ -0,0 +1,80 @@
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event, KeyCode, KeyEvent},
+    style::Style,
+    widgets::{Block, List, ListItem, ListState},
+};
+
+pub struct App {
+    should_exit: bool,
+    items: Vec<String>,
+    state: ListState,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        // https://docs.rs/ratatui/latest/ratatui/widgets/struct.List.html
+        let items = vec![
+            "Item 1".to_string(),
+            "Item 2".to_string(),
+            "Item 3".to_string(),
+        ];
+        let mut state = ListState::default();
+        state.select(Some(0));
+        Self {
+            should_exit: false,
+            items,
+            state,
+        }
+    }
+}
+
+impl App {
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
+        while !self.should_exit {
+            terminal
+                .draw(|frame| draw(frame, &self.items, &mut self.state))
+                .expect("Failed to draw frame");
+
+            if let Event::Key(key) = event::read().unwrap() {
+                self.handle_key(key)?;
+            };
+        }
+
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: KeyEvent) -> color_eyre::Result<()> {
+        match key.code {
+            KeyCode::Up => {
+                let i = self.state.selected().unwrap_or(0);
+                if i > 0 {
+                    self.state.select(Some(i - 1));
+                }
+            }
+            KeyCode::Down => {
+                let i = self.state.selected().unwrap_or(0);
+                if i + 1 < self.items.len() {
+                    self.state.select(Some(i + 1));
+                }
+            }
+            KeyCode::Esc | KeyCode::Char('q') => self.should_exit = true,
+            _ => todo!("handle key"),
+        }
+
+        Ok(())
+    }
+}
+
+fn draw(frame: &mut Frame, items: &[String], state: &mut ListState) {
+    let list_items: Vec<ListItem> = items.iter().map(|s| ListItem::new(s.as_str())).collect();
+    let list = List::new(list_items)
+        .block(Block::bordered().title("List"))
+        .style(Style::new().white())
+        .highlight_style(Style::new().italic())
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true);
+
+    let area = frame.area();
+    frame.render_stateful_widget(list, area, state);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,18 @@
+mod app;
+
+use app::App;
+
 use xapidb_explorer::xapidb::parser::DbNode;
 
-use crossterm::event::{self, Event, KeyCode};
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
 
-use ratatui::{
-    Frame,
-    style::Style,
-    widgets::{Block, List, ListItem, ListState},
-};
-
-fn main() {
     let root = DbNode::read_xml();
     root.print_tree();
 
-    // https://docs.rs/ratatui/latest/ratatui/widgets/struct.List.html
-    let items = ["Item 1", "Item 2", "Item 3"];
-    let mut state = ListState::default();
-    state.select(Some(0));
-
-    let mut terminal = ratatui::init();
-
-    loop {
-        terminal
-            .draw(|frame| draw(frame, &items, &mut state))
-            .expect("Failed to draw frame");
-
-        match event::read().expect("failed to read event") {
-            Event::Key(key) => match key.code {
-                KeyCode::Up => {
-                    let i = state.selected().unwrap_or(0);
-                    if i > 0 {
-                        state.select(Some(i - 1));
-                    }
-                }
-                KeyCode::Down => {
-                    let i = state.selected().unwrap_or(0);
-                    if i + 1 < items.len() {
-                        state.select(Some(i + 1));
-                    }
-                }
-                KeyCode::Esc | KeyCode::Char('q') => break,
-                _ => todo!("handle key"),
-            },
-            _ => {}
-        }
-    }
-
+    let terminal = ratatui::init();
+    let ret = App::default().run(terminal);
     ratatui::restore();
-}
 
-fn draw(frame: &mut Frame, items: &[&str], state: &mut ListState) {
-    let list_items: Vec<ListItem> = items.iter().map(|&i| ListItem::new(i)).collect();
-    let list = List::new(list_items)
-        .block(Block::bordered().title("List"))
-        .style(Style::new().white())
-        .highlight_style(Style::new().italic())
-        .highlight_symbol(">>")
-        .repeat_highlight_symbol(true);
-
-    let area = frame.area();
-    frame.render_stateful_widget(list, area, state);
+    ret
 }
