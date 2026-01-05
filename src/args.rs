18656c716671e5bc11e169d0bd2d2dea000b415c
Guillaume <gthvn1@gmail.com>
2026-01-05 01:24:01 +0100

    Better handling of error when a filename is not provided

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,9 @@
-pub fn get() -> color_eyre::Result<String> {
-    // We are just expecting a filename for now
+pub fn get() -> Result<String, String> {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        color_eyre::eyre::bail!("A filename is expected");
+        Err("expected a filename for the XAPI DB.".to_string())
+    } else {
+        Ok(args[1].to_owned())
     }
-
-    Ok(args[1].to_owned())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,18 @@ use app::App;
 
 use xapidb_explorer::xapidb::parser::DbNode;
 
-struct TerminalDefer;
-impl Drop for TerminalDefer {
-    fn drop(&mut self) {
-        ratatui::restore()
-    }
-}
-
 fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    let terminal = ratatui::init();
-    let _defer = TerminalDefer;
-
-    let filename = args::get()?;
-    let root = DbNode::read_xml(&filename);
-    App::new(root).run(terminal)
+    match args::get() {
+        Ok(filename) => {
+            let terminal = ratatui::init();
+            let root = DbNode::read_xml(&filename);
+            let res = App::new(root).run(terminal);
+            ratatui::restore();
+            res
+        }
+        Err(e) => {
+            println!("Error: {e}");
+            std::process::exit(1);
+        }
+    }
 }
